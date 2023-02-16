c791e77136d7c620dbf5a8cb8bf076db5d063458
m0owo <8breadsticks8@gmail.com>
2023-02-16 22:25:35 +0700

    16/02/23

--- HW/2/2.3.1.cpp
+++ HW/2/2.3.1.cpp
@@ -1,25 +1,64 @@
 #include <iostream>
-#include <random.hpp>
+#include "random.hpp"
 #include <cmath>
+#include <vector>
+#include <fstream>
+#include <string>
 
 double random_n() 
 {
-    Rand_double rnd{0, 1};
+    Rand_double rnd{0, 500};
     std::random_device rd;
     rnd.seed(rd());
     float a = rnd();
     return a;
 }
 
-float calc_dist(float cx = 250, float cy = 250, float x, float y)
+bool in_circle(float x, float y, float cx = 250, float cy = 250)
 {
-    return 0;
+    return (sqrt(pow(x - cx, 2) + pow(y - cy, 2)) <= 250);
 }
 
 int main() 
 {
     int n;
+    float x;
+    float y;
+    std::string line;
+    std::vector<std::string> lines;
     std::cout << "Number of Points: ";
     std::cin >> n;
-    return 0;
+    std::ifstream infile("2.3.svg");
+    while (std::getline(infile, line))
+    {
+        lines.push_back(line);
+    }
+    lines.pop_back();
+
+    std::ofstream circle_file;
+    // circle_file.open("2.3.svg", std::ios::app);
+    circle_file.open("2.3.svg");
+    for (std::string line: lines)
+    {
+        circle_file << line << std::endl;
+    }
+
+    for (int i = 0; i < n; ++i) 
+    {
+        while (true)
+        {
+            x = random_n();
+            y = random_n();
+            if (in_circle(x, y))
+            {
+                break;
+            }
+        }
+        circle_file << "<circle cx=\"" << x << "\" cy=\"" << y << "\" r=\"10\" fill=\"00FFFF\" />" << std::endl;
+    }
+    circle_file << "</svg>" << std::endl;
+    circle_file.close();
+
+
+    //<circle cx="250" cy="250" r="10" fill="#00FFFF" />
 }
